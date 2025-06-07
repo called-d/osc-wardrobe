3907bfb58b5a4b4efc716d70e1ca87448d5db279
called_D <called.0xd@gmail.com>
2025-06-07 22:14:04 +0900

    $ npm run tauri add log

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -13,6 +13,7 @@ fn greet(name: &str) -> String {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_log::Builder::new().build())
         .setup(|app| {
             setup_tray_menu(app)?;
             setup_osc_server(app);
