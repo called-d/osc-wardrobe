f2532510202b8131f9c4f9d5a98964a7c7080592
called_D <called.0xd@gmail.com>
2025-06-08 01:03:03 +0900

    mod: ファイル分割

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,10 +1,8 @@
-use mlua::prelude::LuaResult;
-use mlua::Lua;
-use rosc::{OscMessage, OscPacket};
+mod lua;
+mod osc;
+
 use tauri::tray::{MouseButtonState, TrayIconBuilder, TrayIconEvent};
 use tauri::{App, Manager};
-use vrchat_osc::models::OscRootNode;
-use vrchat_osc::{ServiceType, VRChatOSC};
 
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
 #[tauri::command]
@@ -37,104 +35,16 @@ pub fn run() {
 fn spawn_lua_thread(app: &App) -> Result<(), Box<dyn std::error::Error>> {
     let app_handle = app.app_handle();
     std::thread::spawn(move || {
-        lua_main().unwrap();
+        let engine = lua::LuaEngine::new();
+        let _ = engine.main();
     });
     Ok(())
 }
 
-fn lua_main() -> LuaResult<()> {
-    let lua = Lua::new();
-
-    lua.load("(function() local i = 1; while true do print(i); i = i + 1; end end)()")
-        .exec()?;
-    Ok(())
-}
-
-async fn process_osc() -> Result<(), Box<dyn std::error::Error>> {
-    // Initialize VRChatOSC instance
-    let vrchat_osc = VRChatOSC::new().await?;
-
-    let cloned_vrchat_osc = vrchat_osc.clone();
-    vrchat_osc
-        .on_connect(move |res| match res {
-            ServiceType::Osc(name, addr) => {
-                println!("Connected to OSC server: {} at {}", name, addr);
-                let vrchat_osc = cloned_vrchat_osc.clone();
-                // Send a message to the OSC server
-                tokio::spawn(async move {
-                    vrchat_osc
-                        .send_to_addr(
-                            OscPacket::Message(OscMessage {
-                                addr: "/avatar/parameters/VRChatOSC".to_string(),
-                                args: vec![rosc::OscType::String("Connected".to_string())],
-                            }),
-                            addr,
-                        )
-                        .await
-                        .unwrap();
-                    println!("Sent message to OSC server.");
-                });
-            }
-            ServiceType::OscQuery(name, addr) => {
-                println!("Connected to OSCQuery server: {} at {}", name, addr);
-                let vrchat_osc = cloned_vrchat_osc.clone();
-                // Get parameters from the OSCQuery server
-                tokio::spawn(async move {
-                    // NOTE: When actually retrieving parameters, you should implement retry logic here.
-                    // If VRChat has just started, it is possible that valid values may not be returned immediately.
-                    let params = vrchat_osc
-                        .get_parameter_from_addr("/avatar/parameters", addr)
-                        .await
-                        .unwrap();
-                    println!("Received parameters: {:?}", params);
-                });
-            }
-        })
-        .await;
-
-    // Register a test service
-    let root_node = OscRootNode::new().with_avatar();
-    vrchat_osc
-        .register("test_service", root_node, |packet| {
-            if let OscPacket::Message(msg) = packet {
-                println!("Received OSC message: {:?}", msg);
-            }
-        })
-        .await?;
-    println!("Service registered.");
-
-    // Wait for the service to be registered
-    tokio::time::sleep(tokio::time::Duration::from_secs(2)).await;
-
-    // Send a test message to the registered service
-    vrchat_osc
-        .send(
-            OscPacket::Message(OscMessage {
-                addr: "/chatbox/input".to_string(),
-                args: vec![
-                    rosc::OscType::String("Hello, VRChat!".to_string()),
-                    rosc::OscType::Bool(true),
-                ],
-            }),
-            "VRChat-Client-*",
-        )
-        .await?;
-    println!("Test message sent to VRChat-Client-*.");
-
-    // Get parameters from the registered service
-    let params = vrchat_osc
-        .get_parameter("/avatar/parameters", "VRChat-Client-*")
-        .await?;
-    println!("Received parameters: {:?}", params);
-    //
-    loop {
-        tokio::task::yield_now().await;
-    }
-}
-
 fn setup_osc_server(app: &mut App) {
     let app_handle = app.app_handle();
-    let _osc_handle = tauri::async_runtime::spawn(async move { process_osc().await.unwrap() });
+    let _osc_handle =
+        tauri::async_runtime::spawn(async move { osc::OscService::process_osc().await.unwrap() });
 }
 
 fn setup_tray_menu(app: &mut App) -> Result<(), Box<dyn std::error::Error>> {
