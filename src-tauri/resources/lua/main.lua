77f5d90720a2cd1f6ff834f744e01e1f060c720a
called_D <called.0xd@gmail.com>
2025-06-14 18:41:25 +0900

    mod: osc.send()

--- src-tauri/resources/lua/main.lua
+++ src-tauri/resources/lua/main.lua
@@ -2,6 +2,10 @@
 function main()
     local sec = 5.5
     sleep(sec)
+    local success, err = osc.send("/avatar/change", "avtr_00000000-0000-4000-0000-000000000000")
+    if not success then
+        print("error on avatar change", err)
+    end
     return sec
 end
 
